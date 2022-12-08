bf2f9dead042ac5116f70fe1b7a325eeff116ec9
Marc Verhagen <marc@cs.brandeis.edu>
2022-12-07 19:36:27 -0500

    Importing some old design documents and initial storefront.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# dashboard
+# The CLAMS Dashboard
+
+Experiments with CLAMS dashboards, starting with just an evaluation dashboard.
+
+<img src="docs/workflows/evaluation.png" width=500 align="left">
+
---
+++ code/evaluate.py
@@ -0,0 +1,15 @@
+"""CLAMS Evaluation
+
+Placeholder for top-level evaluation code.
+
+Other modules needed:
+
+- evaluation measures
+- data handling
+- hooks to visualization (submodule or pip install?)
+- hooks to pipeline runner
+
+There is a good case to be made to have some of these as part of a mmif-utils
+package.
+
+"""
